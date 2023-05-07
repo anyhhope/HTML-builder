4ff82bb020b2b44e43aa5bf696fa3190909b2415
anyhhope <hopeanysimova@yandex.ru>
2023-05-07 22:11:29 +0300

    feat: implement 04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const pathToFolder = path.join(__dirname, "secret-folder");
 
-fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
+fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => { //files from folder
     if (err) console.log(err);
     else {
         files.forEach(file => {
-            fs.stat(path.join(pathToFolder, file.name), (err, st) => {
+            fs.stat(path.join(pathToFolder, file.name), (err, st) => { //stats of each file
                 if (err) console.log(err);
                 else if (file.isFile() === true) {
                     console.log(file.name.split('.')[0] + ' - ' + path.extname(file.name).slice(1) + ' - ' + st.size / 1024 + 'kb');
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.readdir(__dirname, { withFileTypes: true }, (err, files) => { //files from folder
+    if (err) console.log(err);
+    else {
+        files.forEach(file => {
+            if (file.isDirectory() && file.name === 'files') {
+                const originalPath = path.join(__dirname, file.name);
+                const newPath = path.join(__dirname, file.name + '-copy');
+
+                fs.promises.mkdir(newPath, { recursive: true }); //create new folder
+                fs.readdir(originalPath, { withFileTypes: true }, (err, files) => { //files from new folder
+                    if (err) console.log(err);
+                    else {
+                        files.forEach(file => {
+                            fs.promises.copyFile(path.join(originalPath, file.name), path.join(newPath, file.name));
+                        })
+                    }
+                });
+            }
+        })
+    }
+});
